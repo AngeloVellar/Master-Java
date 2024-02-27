cc446ffd2fa5342ae9802da837676f66cbefdd8d
Angelo <angelovellar24@gmail.com>
2024-02-27 17:37:22 -0300

    Cj

--- Arreglos/src/EjemploDeterminarOrdenamiento.java
+++ Arreglos/src/EjemploDeterminarOrdenamiento.java
@@ -1,5 +1,28 @@
-public class EjemploDeterminarOrdenamiento {
-    public static void main(String[] args) {
+import java.util.Arrays;
+import java.util.Random;
 
+public class EjemploDeterminarOrdenamiento {
+            // Guardar 20 números. Determinar si está ordenado.
+    public static void main(String[] args) {
+        Random random = new Random();
+        int numeros[] = new int[20];
+
+
+        for(int i = 0; i < numeros.length; i++ ){
+            int randoms = random.nextInt(100);
+            numeros[i] = randoms;
+            System.out.println(numeros[i]);
+        }
+
+        //Arrays.sort(numeros);
+       boolean ban = true;
+        int max = numeros[0];
+        for(int i = 0; i < numeros.length; i++ ){
+            if(numeros[i] >= max){
+                max = numeros[i];
+                System.out.println(numeros[i]);
+            }
+
+        }
     }
 }
