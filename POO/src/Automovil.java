a864297ecd0ff86ac4b962316fefac757a478457
Angelo <angelovellar24@gmail.com>
2024-02-27 21:56:18 -0300

    cambios poo

--- POO/src/Automovil.java
+++ POO/src/Automovil.java
@@ -3,4 +3,33 @@ public class Automovil {
     String color;
     String modelo;
     double cilindrada;
+    int capacidadDeltanque = 40;
+
+    public String verDetalle(){
+        return "Fabricante = " + this.fabricante +
+                "Cilindrada" + this.cilindrada +
+                "Color" + this.color +
+                "Modelo" + this.modelo;
+    }
+    public String acelerar(int rpm){
+        return "el auto" + " " + fabricante +" " + "acelerando a " + rpm + "rpm";
+    }
+
+    public String frenar(){
+        return fabricante + " " + modelo + " " + "frenando";
+    }
+
+    public String acelerarFrenar(int rpm){
+        String acelerar = this.acelerar(rpm);
+        String frenar = this.frenar();
+        return acelerar + "\n" + frenar;
+
+    }
+
+    public float calcularConsumo(int km, float porcentajeBencina ){
+        return km/(capacidadDeltanque*porcentajeBencina);
+    }
+    public float calcularConsumo(int km, int porcentajeBencina ){
+        return km/(capacidadDeltanque*(porcentajeBencina / 100f));
+    }
 }
--- POO/src/EjemploAutomovil.java
+++ POO/src/EjemploAutomovil.java
@@ -1,9 +1,18 @@
 public class EjemploAutomovil {
     public static void main(String[] args) {
-        Automovil auto =  new Automovil();
+        Automovil ford =  new Automovil();
 
-        auto.fabricante = "ford";
+        ford.fabricante = "ford";
+        ford.cilindrada = 2.0;
+        ford.color = "Verde";
+        ford.modelo = "Focus";
 
-        System.out.println(auto.fabricante);
+        System.out.println(ford.verDetalle());
+        System.out.println(ford.acelerar(3000));
+        System.out.println(ford.frenar());
+        System.out.println(ford.acelerarFrenar(5000));
+        System.out.println("Kilometros por litro " + ford.calcularConsumo(300,0.75f));
+        System.out.println("Kilometros por litro " + ford.calcularConsumo(300,60));
+
     }
 }
