eebdd7ad3f385a415fbb9958b6ae3392d456d9e8
Angelo <angelovellar24@gmail.com>
2024-04-23 17:51:20 -0300

    Update: herencia tarea 1

---
+++ HerenciaEjercicio1/PersonasDeCompania/src/Ejemplo.java
@@ -0,0 +1,8 @@
+public class Ejemplo {
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Jorge","Perez","243","San Julian");
+
+        persona1.toString();
+
+    }
+}
---
+++ HerenciaEjercicio1/PersonasDeCompania/src/Gerente.java
@@ -0,0 +1,23 @@
+public class Gerente extends Empleado {
+
+    private Double presupuesto;
+
+    public Gerente(String nombre, String apellido, String numeroFiscal, String direccion,
+                   Double remuneracion, int empleadoId, Double presupuesto) {
+        super(nombre, apellido, numeroFiscal, direccion, remuneracion, empleadoId);
+        this.presupuesto = presupuesto;
+    }
+
+    public Double getPresupuesto() {
+        return presupuesto;
+    }
+    public void setPresupuesto(double presupuesto){
+        this.presupuesto = presupuesto;
+    }
+    @Override
+    public String toString() {
+        return "presupuesto = " + presupuesto + ", " + super.toString();
+    }
+}
+
+
