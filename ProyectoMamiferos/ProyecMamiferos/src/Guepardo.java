10caa9dd674e0e6bcb2e15e626ab2898f8f80e1b
Angelo Vellar <angelovellar24@gmail.com>
2024-05-03 17:17:43 -0300

    "Update: agregando clases"

---
+++ ProyectoMamiferos/ProyecMamiferos/src/Guepardo.java
@@ -0,0 +1,3 @@
+public class Guepardo extends Felino{
+
+}
---
+++ ProyectoMamiferos/ProyecMamiferos/src/Leon.java
@@ -0,0 +1,21 @@
+public class Leon extends Felino{
+    @Override
+    public String comer() {
+        return "";
+    }
+
+    @Override
+    public String dormir() {
+        return "";
+    }
+
+    @Override
+    public String correr() {
+        return "";
+    }
+
+    @Override
+    public String comunicarse() {
+        return "";
+    }
+}
