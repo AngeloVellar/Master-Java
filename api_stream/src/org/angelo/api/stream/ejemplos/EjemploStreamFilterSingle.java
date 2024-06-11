5f66df701ca0ce15bbf9532d008f9d792e15474f
Angelo Vellar <angelovellar24@gmail.com>
2024-06-10 21:21:01 -0300

    Update: Agregando cambios Master-Java

--- api_stream/src/org/angelo/api/stream/ejemplos/EjemploStreamFilterSingle.java
+++ api_stream/src/org/angelo/api/stream/ejemplos/EjemploStreamFilterSingle.java
@@ -13,7 +13,7 @@ public class EjemploStreamFilterSingle {
          Stream<Usuario> nombres = Stream
                  .of("Pato V","Paco R","Pepe H","Pepe G","Luz R")
                  .map(nombre -> new Usuario(nombre.split(" ")[0], nombre.split(" ")[1]))
-                 .filter(u -> u.getNombre().equals("Pepe"))
+                 .filter(u -> u.getNombre().equals("Pepex"))
                  .peek(System.out::println);
 
          Optional<Usuario> usuario = nombres.findFirst();
